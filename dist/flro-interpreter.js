dfb1bc744af9be37651a85ec5f0ac2d6da49d131
Unzor <71789103+Unzor@users.noreply.github.com>
2021-08-20 12:14:57 -0500

    Create flro-interpreter.js

---
+++ dist/flro-interpreter.js
@@ -0,0 +1,207 @@
+var _0x32bb11 = _0x9357;
+(function(_0x4df24b, _0x49ab41) {
+	var _0xea8e45 = _0x9357,
+		_0x54c6db = _0x4df24b();
+	while (!![]) {
+		try {
+			var _0xbd93a3 = parseInt(_0xea8e45(0x148)) / 0x1 * (-parseInt(_0xea8e45(0x141)) / 0x2) + parseInt(_0xea8e45(0x13c)) / 0x3 + parseInt(_0xea8e45(0x120)) / 0x4 + -parseInt(_0xea8e45(0x132)) / 0x5 * (parseInt(_0xea8e45(0x15f)) / 0x6) + -parseInt(_0xea8e45(0x131)) / 0x7 + -parseInt(_0xea8e45(0x13f)) / 0x8 + -parseInt(_0xea8e45(0x16f)) / 0x9 * (-parseInt(_0xea8e45(0x14b)) / 0xa);
+			if (_0xbd93a3 === _0x49ab41) break;
+			else _0x54c6db['push'](_0x54c6db['shift']());
+		} catch (_0x403f6a) {
+			_0x54c6db['push'](_0x54c6db['shift']());
+		}
+	}
+}(_0x26ae, 0x48502));
+var tunnel = {},
+	userName = document[_0x32bb11(0x15d)](_0x32bb11(0x165))['innerHTML'];
+
+function _0x26ae() {
+	var _0x51428f = ['PrintInChat', 'chat-user', 'Smelly\x20Toast', '!record', 'What\x20is\x20the\x20the\x20URL\x20to\x20the\x20script\x20you\x20want\x20to\x20add?', 'changeName', 'vote-yes', '999999999', 'position', 'observe', 'tunnel.', '477dneKCm', '!voteyes', 'click', '!voteno', '\x20unignored.', 'Fylrobot', 'chat', 'Video\x20of\x20VM\x20will\x20be\x20downloaded.\x20Message\x20by\x20Fylrobot,\x20at\x20unzor.github.io/FylrobotJS/', 'use\x20strict', 'ondataavailable', 'splice', 'none', 'forEach', 'Turnspam\x20is\x20on.\x20Message\x20by\x20Fylrobot,\x20at\x20unzor.github.io/FylrobotJS.', 'log', 'style', '\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20', 'data-available', 'indexOf', '!custommessage', 'bottom', '.username', 'download', 'childList', 'vote-no', 'replaceAll', 'CollabVM:', 'Banana\x20Shooter', 'href', 'floor', 'Flying\x20Pigs', 'URL', 'body', 'keyCode', 'push', 'start', 'data', 'What\x20would\x20you\x20like\x20to\x20spam?', 'display:\x20none', 'revokeObjectURL', 'reload', 'vote', '!screenshot', 'stop', 'ctrlKey', 'video/webm', 'head', 'split', '1578020zieBvp', 'addEventListener', 'includes', 'type', '42%', 'querySelector', 'black', 'pop', 'video/webm;\x20codecs=vp9', 'opacity', 'text/css', 'Want\x20Fylrobot\x20for\x20yourself?\x20Go\x20to\x20unzor.github.io/FylrobotJS\x20to\x20get\x20it.', 'User\x20', '!ignore', '_instance_', 'CollabVM', 'call', '3610908LoWfja', '5rXSWNF', '\x20body\x20{\x20background-color:\x20#222;\x20}\x20.navbar>.container-fluid,\x20.thumbnail\x20{\x20background-color:\x20#333\x20!important;\x20background-image:\x20none\x20!important;\x20}\x20.btn\x20{\x20background-color:\x20#333\x20!important;\x20background-image:\x20none\x20!important;\x20border-color:\x20#444\x20!important;\x20color:\x20#ccc\x20!important;\x20text-shadow:\x20none\x20!important;\x20}\x20.list-group-item.disabled\x20{\x20background-color:\x20#444\x20!important;\x20color:\x20#ccc;\x20filter:\x20none;\x20}\x20.list-group-item\x20{\x20background-color:\x20black;\x20}\x20#chat-panel,\x20#chat-input,\x20#chat-user,\x20.guac-keyboard-disabled,\x20.modal-content,\x20.alert\x20{\x20color:\x20white;\x20background-color:\x20#111;\x20}\x20.message-pane\x20li\x20{\x20border-bottom:\x201px\x20solid\x20#333;\x20box-shadow:\x200\x201px\x200\x200\x20#666;\x20}\x20.navbar,\x20.page-header,\x20.thumbnail\x20{\x20border-color:\x20#444\x20!important;\x20border-bottom-color:\x20#444\x20!important;\x20}\x20.navbar-brand,\x20.navbar-collapse>ul>li>a,\x20.page-header>h2,\x20#vm-list,\x20.thumbnail>.caption>h4\x20{\x20color:\x20#ddd\x20!important;\x20}\x20.username::before\x20{\x20color:\x20#fff;\x20}\x20.username,\x20.message-pane\x20.username\x20{\x20color:\x20white;\x20}\x20.input-group-addon,\x20.form-control\x20{\x20border-color:\x20#333;\x20}\x20.list-group-item\x20{\x20border-color:\x20#444;\x20}\x20.panel\x20{\x20border-color:\x20#444;\x20}\x20.message-pane\x20li:hover\x20{\x20background-color:\x20#2b2b2b;\x20}\x20.has-turn.list-group-item\x20{\x20background-color:\x20#365c6b;\x20color:\x20white;\x20}\x20.waiting-turn.list-group-item\x20{\x20background-color:\x20#66662C;\x20color:\x20white;\x20}\x20.alert-info\x20{\x20background-image:\x20none;\x20border-color:\x20#434343;\x20color:\x20white;\x20}\x20', 'captureStream', 'takeTurn', 'keydown', '!darkmode', 'display', 'chat-input', 'tunnel.chat(', 'addedNodes', '63078YUJkkD', 'createElement', '!customscript', '1910968vdJXZq', '!turnspam', '96830FMhIQi', 'Code', '!spam', '!rapidname', 'stopping', 'attributes', 'white', '3NnZxPV', '!unignore\x20', 'username-ok-btn', '184890WENara', 'innerHTML', 'get\x20crashed', 'Spam\x20turned\x20on.\x20To\x20stop\x20spam,\x20press\x20CTRL+Q.\x20Message\x20by\x20Fylrobot,\x20at\x20unzor.github.io/FylrobotJS.', 'left', 'toDataURL', 'For\x20full\x20list\x20of\x20Fylrobot\x20commands,\x20see\x20unzor.github.io/FylrobotJS/commands.txt.', 'createObjectURL', 'turn-btn', '!randommessage', '!ignore\x20', 'username-box', 'appendChild', '000', 'color', 'fixed', '!autotype', '!advertise', 'getElementById', '!help', '1195764gSixyr', 'random', 'Note:\x20to\x20stop\x20spam,\x20press\x20CTRL+Q.', 'PrintInChat(', 'remove'];
+	_0x26ae = function() {
+		return _0x51428f;
+	};
+	return _0x26ae();
+}
+tunnel[_0x32bb11(0x169)] = function(_0x4fc31d) {
+	var _0x560094 = _0x32bb11,
+		_0x3d077c = document[_0x560094(0x15d)](_0x560094(0x156)),
+		_0x5ca644 = document[_0x560094(0x15d)](_0x560094(0x14a));
+	_0x3d077c['value'] = _0x4fc31d, _0x5ca644[_0x560094(0x171)]();
+}, tunnel[_0x32bb11(0x175)] = function(_0x3aa5a7) {
+	var _0x3c3dde = _0x32bb11,
+		_0x4056c2 = document[_0x3c3dde(0x15d)](_0x3c3dde(0x139));
+	_0x4056c2['value'] = _0x3aa5a7, document[_0x3c3dde(0x15d)]('chat-send-btn')['click']();
+}, tunnel[_0x32bb11(0x135)] = function() {
+	var _0xf23ba1 = _0x32bb11;
+	document['getElementById'](_0xf23ba1(0x153))[_0xf23ba1(0x171)]();
+}, tunnel['vote'] = function(_0x3be95b) {
+	var _0x5809bb = _0x32bb11,
+		_0x35bb70 = document[_0x5809bb(0x15d)](_0x5809bb(0x16a)),
+		_0x583420 = document[_0x5809bb(0x15d)](_0x5809bb(0x187));
+	_0x3be95b === 0x0 && (_0x35bb70[_0x5809bb(0x171)](), _0x3be95b === 0x1 && _0x583420[_0x5809bb(0x171)]());
+};
+
+function getInput(_0x21fe59) {
+	var _0x4e667c = _0x32bb11;
+	_0x21fe59 === '!fuckerscript' && (_0x21fe59 = '', tunnel[_0x4e667c(0x169)](_0x4e667c(0x174)), tunnel['chat'](_0x4e667c(0x14d)), tunnel[_0x4e667c(0x169)](name), crash());
+	_0x21fe59 === _0x4e667c(0x140) && (_0x21fe59 = '', tunnel[_0x4e667c(0x169)](_0x4e667c(0x174)), tunnel[_0x4e667c(0x175)](_0x4e667c(0x17c)), tunnel[_0x4e667c(0x169)](name), setInterval(function() {
+		var _0x50949c = _0x4e667c;
+		tunnel[_0x50949c(0x135)]();
+	}, 0xa));
+	if (_0x21fe59 === _0x4e667c(0x143)) {
+		_0x21fe59 = '', tunnel[_0x4e667c(0x169)](_0x4e667c(0x174)), tunnel[_0x4e667c(0x175)](_0x4e667c(0x14e)), tunnel[_0x4e667c(0x169)](userName);
+		var _0x4c3380 = prompt(_0x4e667c(0x115)),
+			_0x3d3066 = _0x4c3380 + _0x4e667c(0x17f),
+			_0x40ecd2 = setInterval(function() {
+				var _0x1c3408 = _0x4e667c;
+				tunnel[_0x1c3408(0x175)](_0x4c3380);
+			}, 0x3e8),
+			_0x3d7b7d = setInterval(function() {
+				var _0x274f94 = _0x4e667c;
+				tunnel[_0x274f94(0x175)](_0x3d3066);
+			}, 0x9c4);
+		document[_0x4e667c(0x121)](_0x4e667c(0x136), function(_0x65a71f) {
+			var _0x36ee09 = _0x4e667c;
+			_0x65a71f[_0x36ee09(0x11c)] && _0x65a71f[_0x36ee09(0x111)] == 0x51 && (clearInterval(_0x40ecd2), clearInterval(_0x3d7b7d));
+		});
+		var _0x36c200 = document['createElement']('div');
+		_0x36c200[_0x4e667c(0x17e)]['backgroundColor'] = _0x4e667c(0x126), _0x36c200[_0x4e667c(0x17e)][_0x4e667c(0x159)] = _0x4e667c(0x147), _0x36c200[_0x4e667c(0x14c)] = _0x4e667c(0x161), _0x36c200[_0x4e667c(0x17e)][_0x4e667c(0x16c)] = _0x4e667c(0x15a), _0x36c200[_0x4e667c(0x17e)][_0x4e667c(0x183)] = '90%', _0x36c200[_0x4e667c(0x17e)][_0x4e667c(0x14f)] = _0x4e667c(0x124), _0x36c200[_0x4e667c(0x17e)]['zIndex'] = _0x4e667c(0x16b), document[_0x4e667c(0x110)][_0x4e667c(0x157)](_0x36c200), setTimeout(function() {
+			var _0x1440fc = _0x4e667c;
+			_0x36c200['style'][_0x1440fc(0x138)] = _0x1440fc(0x17a);
+		}, 0xfa0);
+	};
+	_0x21fe59 === _0x4e667c(0x144) && (_0x21fe59 = '', tunnel[_0x4e667c(0x169)](_0x4e667c(0x174)), tunnel[_0x4e667c(0x175)]('Rapid\x20name\x20turned\x20on.'), tunnel['changeName'](name), setInterval(function() {
+		var _0x2544d3 = _0x4e667c;
+		tunnel[_0x2544d3(0x169)](Math[_0x2544d3(0x18c)](Math['random']() * 0x186a0));
+	}, 0xa));
+	if (_0x21fe59 === _0x4e667c(0x154)) {
+		_0x21fe59 = '';
+		var _0x2cd565 = [_0x4e667c(0x18a), _0x4e667c(0x166), _0x4e667c(0x18d)],
+			_0x570c8 = Math['floor'](Math[_0x4e667c(0x160)]() * 0x4);
+		tunnel[_0x4e667c(0x169)](_0x4e667c(0x174)), tunnel[_0x4e667c(0x175)](_0x2cd565[_0x570c8]), tunnel[_0x4e667c(0x169)](name);
+	}
+	_0x21fe59 === '!breakkeyboard' && (_0x21fe59 = '', tunnel[_0x4e667c(0x169)]('Fylrobot'), tunnel[_0x4e667c(0x175)]('crashed\x20you\x20for\x20being\x20a\x20forkie'), tunnel[_0x4e667c(0x169)](name), crash());
+	if (_0x21fe59 === _0x4e667c(0x11a)) {
+		_0x21fe59 = '';
+		var _0x2754d0 = document[_0x4e667c(0x125)]('canvas')[_0x4e667c(0x150)](),
+			_0x4a070e = document[_0x4e667c(0x13d)]('a');
+		_0x4a070e[_0x4e667c(0x18b)] = _0x2754d0, _0x4a070e[_0x4e667c(0x14c)] = 'a', _0x4a070e[_0x4e667c(0x185)] = _0x4e667c(0x185), _0x4a070e[_0x4e667c(0x17e)][_0x4e667c(0x129)] = '0', document[_0x4e667c(0x110)]['appendChild'](_0x4a070e), _0x4a070e[_0x4e667c(0x171)]();
+	}
+	_0x21fe59 === _0x4e667c(0x15e) && (_0x21fe59 = '', tunnel[_0x4e667c(0x169)](_0x4e667c(0x174)), tunnel[_0x4e667c(0x175)](_0x4e667c(0x151)), tunnel[_0x4e667c(0x169)](name));
+	_0x21fe59 === _0x4e667c(0x15c) && (_0x21fe59 = '', setInterval(function() {
+		var _0x3f3495 = _0x4e667c;
+		tunnel['changeName'](_0x3f3495(0x174)), tunnel[_0x3f3495(0x175)](_0x3f3495(0x12b)), tunnel[_0x3f3495(0x169)](name);
+	}, 0x186a0));
+	if (_0x21fe59 === _0x4e667c(0x182)) {
+		_0x21fe59 = '';
+		var _0x4c3380 = prompt('What\x20would\x20you\x20like\x20to\x20say?');
+		tunnel[_0x4e667c(0x169)](_0x4e667c(0x174)), tunnel[_0x4e667c(0x175)](_0x4c3380), tunnel[_0x4e667c(0x169)](name);
+	};
+	_0x21fe59 === '!disconnect' && (_0x21fe59 = '', location[_0x4e667c(0x118)]());
+	if (_0x21fe59 === _0x4e667c(0x167)) {
+		_0x21fe59 = '';
+		var _0x2f57bb = prompt('How\x20long\x20to\x20record?'),
+			_0x26d28d = _0x2f57bb + _0x4e667c(0x158);
+		tunnel['changeName'](_0x4e667c(0x174)), tunnel[_0x4e667c(0x175)](_0x4e667c(0x176)), tunnel[_0x4e667c(0x169)](name);
+		var _0x132199 = document[_0x4e667c(0x125)]('canvas'),
+			_0x52387b = _0x132199[_0x4e667c(0x134)](0x19),
+			_0x31d6c3 = [];
+		console[_0x4e667c(0x17d)](_0x52387b);
+		var _0x320602 = {
+			'mimeType': _0x4e667c(0x128)
+		};
+		mediaRecorder = new MediaRecorder(_0x52387b, _0x320602), mediaRecorder[_0x4e667c(0x178)] = _0x4ade77, mediaRecorder[_0x4e667c(0x113)]();
+
+		function _0x4ade77(_0xa2dec1) {
+			var _0x317709 = _0x4e667c;
+			console[_0x317709(0x17d)](_0x317709(0x180));
+			if (_0xa2dec1['data']['size'] > 0x0) _0x31d6c3['push'](_0xa2dec1[_0x317709(0x114)]), console['log'](_0x31d6c3), _0xa11969();
+			else {}
+		}
+
+		function _0xa11969() {
+			var _0x417d76 = _0x4e667c,
+				_0x32864f = new Blob(_0x31d6c3, {
+					'type': _0x417d76(0x11d)
+				}),
+				_0x33a92a = URL[_0x417d76(0x152)](_0x32864f),
+				_0x3c853f = document[_0x417d76(0x13d)]('a');
+			document[_0x417d76(0x110)][_0x417d76(0x157)](_0x3c853f), _0x3c853f[_0x417d76(0x17e)] = _0x417d76(0x116), _0x3c853f[_0x417d76(0x18b)] = _0x33a92a, _0x3c853f['download'] = 'vm-recording.mp4', _0x3c853f[_0x417d76(0x171)](), window[_0x417d76(0x18e)][_0x417d76(0x117)](_0x33a92a);
+		}
+		setTimeout(_0x585c27 => {
+			var _0x20b093 = _0x4e667c;
+			console[_0x20b093(0x17d)](_0x20b093(0x145)), mediaRecorder[_0x20b093(0x11b)]();
+		}, _0x26d28d);
+	}
+	if (_0x21fe59 === _0x4e667c(0x13e)) {
+		_0x21fe59 = '';
+		var _0x23ef9a = prompt(_0x4e667c(0x168)),
+			_0x21df79 = document[_0x4e667c(0x13d)]('script');
+		_0x21df79['src'] = _0x23ef9a, document['head'][_0x4e667c(0x157)](_0x21df79), tunnel['changeName'](_0x4e667c(0x174)), tunnel[_0x4e667c(0x175)]('Custom\x20script\x20added.\x20Message\x20by\x20Fylrobot,\x20at\x20unzor.github.io/FylrobotJS.'), tunnel[_0x4e667c(0x169)](name);
+	}
+	if (_0x21fe59 === _0x4e667c(0x137)) {
+		_0x21fe59 = '', _0x4e667c(0x177);
+		var _0x15b860 = document[_0x4e667c(0x13d)](_0x4e667c(0x17e));
+		_0x15b860[_0x4e667c(0x123)] = _0x4e667c(0x12a), _0x15b860[_0x4e667c(0x14c)] = _0x4e667c(0x133), document[_0x4e667c(0x11e)]['appendChild'](_0x15b860);
+	}
+	_0x21fe59 === _0x4e667c(0x15b) && atPrompt();
+	_0x21fe59 === _0x4e667c(0x170) && (_0x21fe59 = '', tunnel[_0x4e667c(0x119)]('vote', '1'));
+	_0x21fe59 === _0x4e667c(0x172) && (_0x21fe59 = '', tunnel[_0x4e667c(0x119)](0x1));
+	if (_0x21fe59[_0x4e667c(0x122)](_0x4e667c(0x12d))) {
+		tunnel[_0x4e667c(0x169)]('Fylrobot'), tunnel[_0x4e667c(0x175)](_0x4e667c(0x12c) + _0x21fe59['split'](_0x4e667c(0x155))[_0x4e667c(0x127)]() + '\x20ignored.\x20Use\x20!unignore\x20to\x20unhide\x20the\x20person.'), tunnel[_0x4e667c(0x169)](name);
+		var _0x42b460 = document[_0x4e667c(0x110)],
+			_0x5b9524 = {
+				'attributes': !![],
+				'childList': !![],
+				'subtree': !![]
+			},
+			_0x51840a = function(_0x3c3e3f, _0x34e286) {
+				var _0x38b039 = _0x4e667c;
+				for (const _0x552f90 of _0x3c3e3f) {
+					if (_0x552f90['type'] === _0x38b039(0x186)) ignoredUsers[_0x38b039(0x17b)](function(_0x13db66) {
+						var _0x53aa93 = _0x38b039;
+						_0x552f90[_0x53aa93(0x13b)][0x0][_0x53aa93(0x125)](_0x53aa93(0x184))['innerHTML'] === _0x13db66 && (document[_0x53aa93(0x15d)]('chat-sound-btn')[_0x53aa93(0x171)](), _0x552f90['addedNodes'][0x0][_0x53aa93(0x163)](), document['getElementById']('chat-sound-btn')[_0x53aa93(0x171)]());
+					});
+					else {
+						if (_0x552f90['type'] === _0x38b039(0x146)) {}
+					}
+				}
+			},
+			_0x3b7ca2 = new MutationObserver(_0x51840a);
+		_0x3b7ca2[_0x4e667c(0x16d)](_0x42b460, _0x5b9524), ignoredUsers[_0x4e667c(0x112)](_0x21fe59[_0x4e667c(0x11f)](_0x4e667c(0x155))[_0x4e667c(0x127)]()), _0x21fe59 = '';
+	};
+	_0x21fe59[_0x4e667c(0x122)]('!unignore') && (ignoredUsers[_0x4e667c(0x179)](ignoredUsers[_0x4e667c(0x181)](_0x21fe59[_0x4e667c(0x11f)](_0x4e667c(0x149))[_0x4e667c(0x127)]())), tunnel[_0x4e667c(0x169)](_0x4e667c(0x174)), tunnel[_0x4e667c(0x175)]('User\x20' + _0x21fe59[_0x4e667c(0x11f)](_0x4e667c(0x149))[_0x4e667c(0x127)]() + _0x4e667c(0x173)), tunnel[_0x4e667c(0x169)](name));
+}
+
+function _0x9357(_0x4b1a21, _0x5c1090) {
+	var _0x26aee5 = _0x26ae();
+	return _0x9357 = function(_0x9357e2, _0x39c3c1) {
+		_0x9357e2 = _0x9357e2 - 0x110;
+		var _0x22cd30 = _0x26aee5[_0x9357e2];
+		return _0x22cd30;
+	}, _0x9357(_0x4b1a21, _0x5c1090);
+};
+var keywords = [_0x32bb11(0x174), _0x32bb11(0x164), _0x32bb11(0x12f)],
+	Fylrobot = {
+		'RunCommand': function(_0x5a86c8) {
+			getInput(_0x5a86c8);
+		}
+	},
+	FlroInterpreter = {
+		'run': function(_0x24de20) {
+			var _0x117390 = _0x32bb11;
+			FlroInterpreter[_0x117390(0x12e)] = {
+				'Code': _0x24de20
+			}, evalInContext[_0x117390(0x130)]({
+				'PrintInChat': function(_0x4b284b) {
+					tunnel['chat'](_0x4b284b);
+				},
+				'CollabVM': tunnel
+			});
+		}
+	};
+
+function evalInContext() {
+	var _0x4b05bc = _0x32bb11;
+	eval(FlroInterpreter[_0x4b05bc(0x12e)][_0x4b05bc(0x142)][_0x4b05bc(0x188)](_0x4b05bc(0x162), _0x4b05bc(0x13a))[_0x4b05bc(0x188)](_0x4b05bc(0x189), _0x4b05bc(0x16e)));
+}
